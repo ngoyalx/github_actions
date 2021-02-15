5801fa578bb965943d1d3bda484e815db7c572d8
Nitin Goyal <n.goyal@dnata.com>
2021-02-15 15:51:30 +0400

    first commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    public void firstTestCase(){
+        System.out.println("This is a demo test case to check github actions");
+    }
+}
