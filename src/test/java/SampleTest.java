61aa3ff9cb1a2e44a0fee5bea8123b4a95636814
Nitin Goyal <n.goyal@dnata.com>
2021-02-15 16:20:42 +0400

    changed the file again

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -6,5 +6,6 @@ public class SampleTest {
     public void firstTestCase(){
         System.out.println("This is a demo test case to check github actions");
         System.out.println("Changed the file to check github actions");
+        System.out.println("Another push to check github actions");
     }
 }
